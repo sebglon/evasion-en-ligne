4f526047f692f1f48c1891bfc1aa0bab32ce37c0
sebastien.glon <sebastien.glon@netapsys.fr>
2013-08-18 19:05:54 +0200

    ajout du block subviewLink  et de la récursivité

--- web/src/main/webapp/js/controllers.js
+++ web/src/main/webapp/js/controllers.js
@@ -5,13 +5,20 @@ angular.module('evasionVisiteurApp.controllers', []).
         Site.get().then(function(response) {
             $scope.site = response;
             console.log(response);
-            angular.forEach(response.views, function(view) {
-                $route.routes['/' + view.url] = {templateUrl: 'partials/basic-page.html', controller: function($rootScope) {
-                        console.log('test '+ angular.toJson(view.blocs));
-                        $rootScope.view = view;
+            function createRoute(views) {
+                angular.forEach(views, function(view) {
+                    $route.routes['/' + view.url] = {templateUrl: 'partials/basic-page.html', controller: function($rootScope) {
+                            console.log('test ' + angular.toJson(view.blocs));
+                            $rootScope.view = view;
+                        }
+                    };
+                    if (view.views !== undefined) {
+                        createRoute(view.views);
                     }
-                };
-            });
+                });
+            }
+            ;
+            createRoute(response.views);
         });
         $scope.$location = $location;
     }]);
