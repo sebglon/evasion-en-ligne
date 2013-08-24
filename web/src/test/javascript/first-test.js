33ef82efdd0a81d4119f69987f7d73333f69bacb
sebastien.glon <sebastien.glon@netapsys.fr>
2013-08-25 00:26:11 +0200

    Mise en place de la compression des JS et CSS; Ajout du plugin Jasmine pour les TU Javascript;

---
+++ web/src/test/javascript/first-test.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+describe('The quickstart object',function()
+{
+    it('adds two numbers',function()
+    {
+        var result = DE_AKQUINET.quickstart.addNumbers(1, 4);
+        expect(result).toBe(5);
+    });
+});
