a84e9f7e9823e32dd25c648151f30d0bd190aed0
sebastien.glon <sebastien.glon@netapsys.fr>
2013-08-24 22:29:08 +0200

    Déplacement du chargement du descripteur de site dans le run app

--- web/src/main/webapp/js/app.js
+++ web/src/main/webapp/js/app.js
@@ -1,13 +1,48 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var evasionVisiteurApp = angular.module('evasionVisiteurApp', ['evasionVisiteurApp.services','evasionVisiteurApp.siteServices', 'evasionVisiteurApp.controllers']);
+var evasionVisiteurApp = angular.module('evasionVisiteurApp', ['restangular']);
 
-evasionVisiteurApp.config(['$routeProvider', function($routeProvider) {
-       // $routeProvider.when('/', {templateUrl: 'partials/basic-page.html'});
+evasionVisiteurApp.config(['$routeProvider', 'RestangularProvider', function($routeProvider, RestangularProvider) {
+        // $routeProvider.when('/', {templateUrl: 'partials/basic-page.html'});
         //$routeProvider.when('/booktravel', {templateUrl: 'partials/booktravel.html'});
         $routeProvider.otherwise({redirectTo: '/'});
+        RestangularProvider.setBaseUrl("default-data");
     }]);
+
+angular.injector(['ng', 'restangular']).invoke(function(Restangular, $rootScope) {
+    evasionVisiteurApp.run(function($rootScope, Restangular, $route, $location) {
+        Restangular.one('site.json').get().then(function(response) {
+            $rootScope.site = response;
+            console.log(response);
+            function createRoute(views) {
+                angular.forEach(views, function(view) {
+                    var route = { controller: function($rootScope) {
+                            console.log('test ' + angular.toJson(view.blocs));
+                            $rootScope.view = view;
+                        }
+                    };
+                    if (view.content !== undefined) {
+                        route.template = view.content;
+                    } else if (view.template !== undefined) {
+                        route.template = view.content;
+                    } else {
+                        route.templateUrl = view.templateUrl;
+                    }
+                    $route.routes[view.url] = route;
+                    if (view.views !== undefined) {
+                        createRoute(view.views);
+                    }
+                });
+            }
+            ;
+            createRoute(response.views);
+            $route.reload();
+        });
+        $rootScope.$location = $location;
+    });
+}
+);
 
 angular.injector(['ng']).invoke(function($rootScope) {
     $rootScope.$on('$routeChangeStart', function($event, currentRoute, previousRoute) {
