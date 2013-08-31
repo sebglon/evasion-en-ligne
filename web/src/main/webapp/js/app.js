c861b02637c9a06f8d09f924e406f43644f2d009
sebastien.glon <sebastien.glon@netapsys.fr>
2013-08-31 23:08:33 +0200

    configuration de l'éditeur de text; ajout du titre de view et de la description;

--- web/src/main/webapp/js/app.js
+++ web/src/main/webapp/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var evasionVisiteurApp = angular.module('evasionVisiteurApp', ['restangular','evasionVisiteurApp.controllers']);
+var evasionVisiteurApp = angular.module('evasionVisiteurApp', ['restangular', 'evasionVisiteurApp.controllers']);
 
 evasionVisiteurApp.config(['$routeProvider', 'RestangularProvider', function($routeProvider, RestangularProvider) {
         // $routeProvider.when('/', {templateUrl: 'partials/basic-page.html'});
@@ -17,7 +17,7 @@ angular.injector(['ng', 'restangular']).invoke(function(Restangular, $rootScope)
             console.log(response);
             function createRoute(views) {
                 angular.forEach(views, function(view) {
-                    var route = { controller: function($rootScope) {
+                    var route = {controller: function($rootScope) {
                             console.log('test ' + angular.toJson(view.blocs));
                             $rootScope.view = view;
                         }
@@ -56,3 +56,93 @@ angular.injector(['ng']).invoke(function($rootScope) {
     });
 }
 );
+
+// tinyMCE directive
+/**
+ * Binds a TinyMCE widget to <textarea> elements.
+ */
+evasionVisiteurApp
+        .value('uiTinymceConfig', {
+    plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+
+
+            browser_spellcheck: true, object_resizing: true, menubar: false, content_css: "/css/app.css, /css/lib/bootstrap/bootstrap.css"
+})
+        .directive('uiTinymce', ['uiTinymceConfig', function(uiTinymceConfig) {
+        uiTinymceConfig = uiTinymceConfig || {};
+        var generatedIds = 0;
+        return {
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ngModel) {
+                var expression, options, tinyInstance,
+                        updateView = function() {
+                    ngModel.$setViewValue(elm.val());
+                    if (!scope.$$phase) {
+                        scope.$apply();
+                    }
+                };
+                // generate an ID if not present
+                if (!attrs.id) {
+                    attrs.$set('id', 'uiTinymce' + generatedIds++);
+                }
+
+                if (attrs.uiTinymce) {
+                    expression = scope.$eval(attrs.uiTinymce);
+                } else {
+                    expression = {};
+                }
+                options = {
+                    // Update model when calling setContent (such as from the source editor popup)
+                    setup: function(ed) {
+                        var args;
+                        ed.on('init', function(args) {
+                            ngModel.$render();
+                        });
+                        // Update model on button click
+                        ed.on('ExecCommand', function(e) {
+                            ed.save();
+                            updateView();
+                        });
+                        // Update model on keypress
+                        ed.on('KeyUp', function(e) {
+                            ed.save();
+                            updateView();
+                        });
+                        // Update model on change, i.e. copy/pasted text, plugins altering content
+                        ed.on('SetContent', function(e) {
+                            if (!e.initial) {
+                                ed.save();
+                                updateView();
+                            }
+                        });
+                        if (expression.setup) {
+                            scope.$eval(expression.setup);
+                            delete expression.setup;
+                        }
+                    },
+                    mode: 'exact',
+                    elements: attrs.id
+                };
+                // extend options with initial uiTinymceConfig and options from directive attribute value
+                angular.extend(options, uiTinymceConfig, expression);
+                setTimeout(function() {
+                    tinymce.init(options);
+                });
+
+
+                ngModel.$render = function() {
+                    if (!tinyInstance) {
+                        tinyInstance = tinymce.get(attrs.id);
+                    }
+                    if (tinyInstance) {
+                        tinyInstance.setContent(ngModel.$viewValue || '');
+                    }
+                };
+            }
+        };
+    }]);
--- web/src/main/webapp/js/controllers.js
+++ web/src/main/webapp/js/controllers.js
@@ -2,22 +2,29 @@
 
 angular.module('evasionVisiteurApp.controllers', []).
         controller('EditStaticContent', ['$scope', function($scope) {
+        $scope.tinymceOptions = {
+            handle_event_callback: function(e) {
+                // put logic here for keypress
+            }
+        };
 
         $scope.edit = function() {
+            $scope.title = angular.copy($scope.view.title);
             $scope.content = angular.copy($scope.view.content);
-            $scope.onEditContent=true;
+            $scope.onEditContent = true;
         }
-        $scope.update = function(content) {
+        $scope.update = function(content, title) {
+            $scope.view.title = angular.copy(title);
             $scope.view.content = angular.copy(content);
-            $scope.onEditContent=false;
+            $scope.onEditContent = false;
         };
 
         $scope.reset = function() {
             $scope.onEditContent = false;
         };
 
-        $scope.isUnchanged = function(content) {
-            return angular.equals(content, $scope.view.content);
+        $scope.isUnchanged = function(content, title) {
+            return angular.equals(content, $scope.view.content) && angular.equals(title, $scope.view.title);
         };
 
         $scope.reset();
